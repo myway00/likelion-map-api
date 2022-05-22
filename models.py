e0abfb6a364858576a8dcd6d4fa85b707e0bd835
eastyun <76711238+myway00@users.noreply.github.com>
2022-05-22 14:35:18 +0900

    drf 2차 세션 (view 실습 - mixins 사용)

--- models.py
+++ models.py
@@ -1,23 +1,15 @@
 from django.db import models
 
-from django.db import models
-from pygments.lexers import get_all_lexers
-from pygments.styles import get_all_styles
+# 블로그는 다양한 포스트 단위로 구분
+# 하나의 포스트에는 제목, 내용, 작성시간 정보가 들어있을 예정
 
-LEXERS = [item for item in get_all_lexers() if item[1]]
-LANGUAGE_CHOICES = sorted([(item[1][0], item[0]) for item in LEXERS])
-#pygment에서 제공하는 언어 종류들 (python, java 등)
-STYLE_CHOICES = sorted([(item, item) for item in get_all_styles()])
-#pygment에서 제공하는 코드 보여주기 옵션들
+# django.db.models.Model 을 상속 받는 클래스를 정의하게 되면 
+# django가  관리하는 하나의 데이터 모델이 생성되는 것
+class Post(models.Model) : 
 
-class Snippet(models.Model):
-    created = models.DateTimeField(auto_now_add=True)
-    title = models.CharField(max_length=100, blank=True, default='')
-    code = models.TextField()
-    linenos = models.BooleanField(default=False) #행번호 보여줄 지 유무
-    language = models.CharField(choices=LANGUAGE_CHOICES, default='python', max_length=100)
-    style = models.CharField(choices=STYLE_CHOICES, default='friendly', max_length=100)
-
-    class Meta:
-        #Meta클래스는 권한, 데이터베이스 이름, 단 복수 이름, 추상화, 순서 지정 등과 같은 모델에 대한 다양한 사항을 정의하는 데 사용
-        ordering = ['created']
+    title = models.CharField(max_length=200)
+    content = models.CharField(max_length=2048)
+    #제목과 내용은 문자열로 된 필드,
+    date = models.DateTimeField(auto_created=True, auto_now=True)
+    #  작성 시간은 ‘자동으로 생성
+    # 현재 시간이 자동으로 기록되는’ DateTime 필드
--- views.py
+++ views.py
@@ -1,3 +1,35 @@
 from django.shortcuts import render
+from rest_framework.generics import GenericAPIView
+from rest_framework import serializers, mixins
+# ListModelMixin 사용 위해 mixins 추가
 
-# Create your views here.
+from blog.models import Post
+
+#blog_api 위에 정의해주세요!
+class PostSerializer(serializers.ModelSerializer) :
+    class Meta : 
+        model = Post
+        fields = '__all__'
+
+class blog_api(GenericAPIView, mixins.ListModelMixin, mixins.CreateModelMixin ):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+    def get(self, request, *args, **kwargs) :
+        return self.list(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
+
+class blog_detail_api(GenericAPIView, mixins.RetrieveModelMixin, mixins.UpdateModelMixin, mixins.DestroyModelMixin):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
+    
+    def put(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
+        
+    def delete(self, request, *args, **kwargs):
+        return self.destroy(request, *args, **kwargs)
